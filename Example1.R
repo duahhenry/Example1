185988ea8760f128e82173df7086be8b737ba2bf
duahhenry <duahhenryofori@gmail.com>
2023-01-12 17:20:01 -0500

    added #text

--- Example1.R
+++ Example1.R
@@ -1,3 +1,4 @@
+#text
 file <- "TCGA_breast_cancer_LumA_vs_Basal_PAM50.tsv"
 first10 <- c('NAT1','BIRC5','BAG1','BCL2','BLVRA','CCNB1','CCNE1','CDC6','CDC20','CDH3')
 nfold <- 3
@@ -5,6 +6,7 @@ nfold <- 3
 header <- scan(file, nlines = 1, sep="\t", what = character())
 data <- read.table(file, skip = 2, header = FALSE, sep = "\t", quote = "", check.names=FALSE)
 names(data) <- header
+
 
 header2 <- scan(file, skip = 1, nlines = 1, sep="\t", what = character())
 
